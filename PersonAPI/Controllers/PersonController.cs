a372d1273078dc70413f70fd86fef1e17d204afa
marcomendez <marco.mendez@communitybrands.com>
2019-09-25 17:57:39 -0400

    added example get

---
+++ PersonAPI/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonAPI.Models;
+using System.Collections.Generic;
+
+namespace PersonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        // GET api/Person
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Welcome", "Cabrones" };
+        }
+
+        //Get
+        [HttpGet]
+        [Route("getpeople")]
+        public ActionResult<IEnumerable<Person>> GetPeople()
+        {
+            List<Person> people = new List<Person>
+            {
+                new Person( 1,"Marco", 15),
+                new Person( 2,"Pedro", 35),
+                new Person( 3,"Juan", 25)
+            };
+            return people;
+        }
+
+    }
+}
---
+++ PersonAPI/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PersonAPI.Models
+{
+    public class Person
+    {
+        public Person(long id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int Age{ get; set; }
+    }
+}
